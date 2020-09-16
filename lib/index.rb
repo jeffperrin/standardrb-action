217ed77798c497e4000f2a6ea428a925ac6bc3d7
Jeff Perrin <jeffperrin@gmail.com>
2020-09-16 10:24:55 -0600

    Try removing the chdir

--- lib/index.rb
+++ lib/index.rb
@@ -22,7 +22,7 @@ end
   if ENV["REPORT_PATH"]
     read_json(ENV["REPORT_PATH"])
   else
-    Dir.chdir(ENV["GITHUB_WORKSPACE"]) { JSON.parse(`standardrb --parallel -f json`) }
+    JSON.parse(`standardrb --parallel -f json`)
   end
 
 GithubCheckRunService.new(@report, @github_data, ReportAdapter).run
